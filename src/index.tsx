889c7056c3dfb515e7fda015c945940e91bc1d37
Michael van Engelshoven <michael@van-engelshoven.de>
2023-01-16 06:44:30 +0100

    Fix missing migration of render call for react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import React from 'react'
 import App from './components/App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const container = document.getElementById('root');
+
+if (!container) {
+    throw Error('Could not find app container to render');
+}
+
+const root = createRoot(container);
+root.render(<App />);
